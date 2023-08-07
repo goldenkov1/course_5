f6feb4a58f04e9210261fb7bf76344a2b43fc9f4
goldenkov slava <oldfild1@gmail.com>
2023-08-08 01:14:36 +0300

    add code

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from pprint import pprint
-employer_id = ['856498', '3591220', '5674346', '1993194', '9417901', '5674346', '2389722', '1535743', '2590583',
-               '2509056']
 
-
-from sql_manager import HH
+from api_hh import HH
 
 
 def main():
-    hh = HH("https://api.hh.ru/vacancies")
-    pprint(hh.filtered_id())
+    pprint(HH().get_employers())
+    #pprint(HH.get_vacancies())
 
 
 
